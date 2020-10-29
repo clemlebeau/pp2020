1ba3f9358eb1ced48e60d5d3f61ee2c41b0f8c34
drlime04 <smileecraft13@gmail.com>
2020-10-29 13:16:18 -0400

    Updated project to new Unity version and changed PlayerController NOTE:There are now two player controllers and compatibility issues between them make the game not work anymore

--- PP2020/Assets/Scripts/PlayerController.cs
+++ PP2020/Assets/Scripts/PlayerController.cs
@@ -1,98 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
+using Unity.Profiling;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D playerRigidBody;
-    private Collider2D playerCollider;
-
-    public float playerMoveSpeed = 1f;
-    public float jumpPower = 5f;
-    public float maxRunSpeed = 5000f;
-    public float standingContactDistance = .1f;
-    public float stopInputThreshold = .5f;
-    public bool capVelocity = true;
+    private CharacterController controller;
 
     void Awake()
     {
-        playerRigidBody = GetComponent<Rigidbody2D>();
-        playerCollider = GetComponent<Collider2D>();
+        controller = GetComponent<CharacterController>();
     }
     void FixedUpdate()
     {
-        if (playerRigidBody != null)
-        {
-            ApplyInput();
-            if (capVelocity)
-            {
-                CapVelocity();
-            }
-            else
-            {
-                Debug.LogWarning("No rigid body at" + gameObject.name);
-            }
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical") ;
+
+        controller.Move(new Vector3(xInput, yInput, 0));
     }
 
-    void ApplyInput()
-    {
-
-        float xInput = Input.GetAxisRaw("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-
-        //Left and right
-        float xForce = Mathf.Sign(xInput) * playerMoveSpeed * Time.deltaTime;
-        float yForce = 0;
-
-        //Jumping
-        if (yInput > 0)
-        {
-            if (IsOnTopCollider() && playerRigidBody.velocity.y == 0)
-            {
-                yForce = jumpPower;
-            }
-        }
-
-        Vector2 force = new Vector2(xForce, yForce);
-
-        playerRigidBody.AddForce(force, ForceMode2D.Impulse);
-
-        if (Mathf.Abs(xInput) <= stopInputThreshold)
-            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
-    }
-
-    void CapVelocity()
-    {
-        float cappedXVelocity = Mathf.Min(Mathf.Abs(playerRigidBody.velocity.x), maxRunSpeed) * Mathf.Sign(playerRigidBody.velocity.x);
-        float cappedYVelocity = playerRigidBody.velocity.y;
-
-        playerRigidBody.velocity = new Vector2(cappedXVelocity, cappedYVelocity);
-    }
-
-    public bool IsOnTopCollider()
-    {
-        if (playerCollider)
-        {
-            ContactFilter2D filter2D = new ContactFilter2D();
-            filter2D.useTriggers = false;
-
-            RaycastHit2D[] results = new RaycastHit2D[10];
-
-            playerCollider.Cast(new Vector2(0, -1), filter2D, results, standingContactDistance);
-
-            foreach (RaycastHit2D hit in results)
-            {
-                if (hit.collider && hit.collider.transform.position.z == playerCollider.transform.position.z)
-                {
-                    return true;
-                }
-            }
-
-        }
-        return false;
-    }
 }
