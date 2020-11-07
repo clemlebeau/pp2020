53e27317c32a17905111d6c6ef93b884ba44b19c
drlime04 <46548560+drlime04@users.noreply.github.com>
2020-11-07 14:42:45 -0500

    Fixing issues with character controller, replacing it with a custom made script

---
+++ PP2020/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,92 @@
+﻿using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
+    float speed = 9;
+
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float walkAcceleration = 75;
+
+    [SerializeField, Tooltip("Acceleration while in the air.")]
+    float airAcceleration = 30;
+
+    [SerializeField, Tooltip("Deceleration applied when character is grounded and not attempting to move.")]
+    float groundDeceleration = 70;
+
+    [SerializeField, Tooltip("Deceleration applied when character is in the air and not attempting to move.")]
+    float airDeceleration = 0;
+
+    [SerializeField, Tooltip("Max height the character will jump regardless of gravity")]
+    float jumpHeight = 4;
+
+    private BoxCollider2D boxCollider;
+
+    private Vector2 velocity;
+
+    private bool grounded;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (grounded)
+        {
+            velocity.y = 0;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                //Calculate the velocity required to achieve the target jump height with a derived known formula
+                velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+            }
+        }
+
+        float acceleration = grounded ? walkAcceleration : airAcceleration;
+        float deceleration = grounded ? groundDeceleration : airDeceleration;
+
+
+        if (moveInput != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+
+        transform.Translate(velocity * Time.deltaTime);
+
+
+        grounded = false;
+
+        //If character is getting pushed into unwanted colliders, re run this multiple times until there are no more collisions.
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+
+        foreach (Collider2D hit in hits)
+        {
+            if (hit == boxCollider)
+                continue;
+            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+
+
+                if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && velocity.y < 0)
+                {
+                    grounded = true;
+                }
+            }
+        }
+    }
+}
