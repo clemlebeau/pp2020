84c7b6bcba7103d48e4611ecf96e2fd182a77f4a
drlime04 <46548560+drlime04@users.noreply.github.com>
2020-11-23 10:41:33 -0500

    Added post-processing package and several p-processing profiles that will be used in pill side effects

--- PP2020/Assets/Scripts/Pill/PillController.cs
+++ PP2020/Assets/Scripts/Pill/PillController.cs
@@ -1,34 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class PillController : MonoBehaviour
 {
     public Camera mainCamera;
+    public GameObject player;
+    private PostProcessVolume postProcessVolume;
+
+    public float minSideEffectDuration = 10f;
+    public float maxSideEffectDuration = 25f;
+    private float postProcessingEffectTime;
+
+    //Post processing profiles
+    public PostProcessProfile defaultProfile;
+    public PostProcessProfile blackAndWhiteProfile;
+
     PlayerTicController ticController;
+    SpriteRenderer spriteRenderer;
     void Awake()
     {
-        ticController = GetComponent<PlayerTicController>();
+        if (mainCamera == null)
+        {
+            Debug.LogWarning("Camera is not defined");
+        }
+        if (player == null)
+        {
+            Debug.LogWarning("Player is not defined");
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ticController = player.GetComponent<PlayerTicController>();
+        postProcessVolume = mainCamera.GetComponent<PostProcessVolume>();
     }
 
     public void ConsumePill()
     {
-        GetComponent<SpriteRenderer>().enabled = false; //Hides the pill to make it look like it was consumed, but it will be destroyed only after the side effects are finished.
-        ticController.ResetTicTime();
-        SideEffects();
+        if (spriteRenderer.enabled)
+        {
+            spriteRenderer.enabled = false; //Hides the pill to make it look like it was consumed, but it will be destroyed only after the side effects are finished.
+            ticController.ResetTicTime();
+            SideEffects();
+        }
     }
 
     private void SideEffects()
     {
-        int sideEffect = Random.Range(0, 5);
+        int sideEffect = Random.Range(0, 0);
         switch (sideEffect)
         {
             case 0:
                 //B&W
-                
+                postProcessVolume.profile = blackAndWhiteProfile;
+                StartPostProcessTimer();
                 break;
             case 1:
                 break;
         }
+    }
+
+    void Update()
+    {
+        if (postProcessingEffectTime > 0)
+        {
+            postProcessingEffectTime -= Time.deltaTime;
+        } else
+        {
+            postProcessVolume.profile = defaultProfile;
+        }
+    }
+
+    private void StartPostProcessTimer()
+    {
+        postProcessingEffectTime = Random.Range(minSideEffectDuration, maxSideEffectDuration);
     }
 }
